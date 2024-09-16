3f0bdd7b0bb286b817f18499432ee5cefdf5089b
ROSHAN KUMAR YADAV <roshan099999@gmail.com>
2024-09-16 17:18:28 +0530

    adding icons and getter funtion of date

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
 
 final uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const CategoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+  Category.travel: Icons.flight_takeoff
+};
+
+final dateFomatter = DateFormat.yMd();
 
 class Expense {
   Expense(
@@ -17,4 +28,9 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  // methods or function
+  String get getFormatDate {
+    return dateFomatter.format(date);
+  }
 }
--- lib/widget/expenses_list/expenses_item.dart
+++ lib/widget/expenses_list/expenses_item.dart
@@ -8,7 +8,31 @@ class ExpensesItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(expense.title),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4,),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(), // take all the avialable space 
+                Row(
+                  children: [
+                     Icon(CategoryIcons[expense.category]),
+                    const SizedBox(width: 8,),
+                    Text(expense.getFormatDate)
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
