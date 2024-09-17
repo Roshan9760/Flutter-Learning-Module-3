a8993498b3a44aa03cb27a321bf3e969284e4452
ROSHAN KUMAR YADAV <roshan099999@gmail.com>
2024-09-17 12:04:08 +0530

    add showDatePicker overlay to pick date

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -10,12 +10,23 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseSatate extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
-  
-  //  to delete the controller after usage 
+  //  to delete the controller after usage
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -29,13 +40,50 @@ class _NewExpenseSatate extends State<NewExpense> {
           TextField(
             maxLength: 50,
             controller: _titleController,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(label: Text('Title')),
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  decoration: const InputDecoration(
+                      label: Text('Amount'), prefixText: '\$'),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Selected Date'),
+                    IconButton(
+                      onPressed: () {
+                        _presentDatePicker();
+                      },
+                      icon: Icon(Icons.calendar_month),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text('Save The Title'),
               )
