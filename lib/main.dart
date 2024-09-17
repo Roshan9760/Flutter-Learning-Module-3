0fc610290a391c2350c8ae3649b9191b6931c1dc
ROSHAN KUMAR YADAV <roshan099999@gmail.com>
2024-09-17 17:35:38 +0530

    adding color and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:expensetrackerapp/widget/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
 void main() {
-  runApp( MaterialApp(
-    theme: ThemeData(useMaterial3: true),
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer
+          )
+          ),
       debugShowCheckedModeBanner: false,
-      home:const Exprenses(),
+      home: const Exprenses(),
     ),
   );
 }
--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -37,6 +37,7 @@ class _ExprensesSatate extends State<Exprenses> {
       _registeredExpensed.remove(expense);
     });
 
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       duration: const Duration(seconds: 5),
       content: const Text('Expense Deleted !'),
